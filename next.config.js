ad52d7277993ac53679a0ff45ad9cb4ec3262dee
inderis-mendoza <contacto.idsservicios@gmail.com>
2026-03-26 19:32:51 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true }
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
